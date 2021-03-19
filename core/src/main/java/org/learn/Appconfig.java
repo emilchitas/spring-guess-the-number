a6e3bf5c8bb047fd4706d83d0629a74ca1e94c90
Emil Chitas <emil.chitas@dynatrace.com>
2021-03-19 13:27:00 +0100

    Configuration Bean

--- core/src/main/java/org/learn/Appconfig.java
+++ core/src/main/java/org/learn/Appconfig.java
@@ -1,9 +1,18 @@
 package org.learn;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "org.learn")
 public class Appconfig {
+    @Bean
+    public NumberGenerator numberGenerator(){
+        return new NumberGeneratorImpl();
+    }
+    @Bean
+    public Game game(){
+        return new GameImpl();
+    }
 }
--- core/src/main/java/org/learn/NumberGeneratorImpl.java
+++ core/src/main/java/org/learn/NumberGeneratorImpl.java
@@ -1,10 +1,8 @@
 package org.learn;
 
-import org.springframework.stereotype.Component;
-
 import java.util.Random;
 
-@Component("generator")
+
 public class NumberGeneratorImpl implements NumberGenerator {
     private final Random random = new Random();
     private int maxNumber = 100;
