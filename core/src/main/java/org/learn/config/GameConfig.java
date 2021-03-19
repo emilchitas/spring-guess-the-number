4e141d8e4317e8b7cd1291870b40d697faca7cdb
Emil Chitas <emil.chitas@dynatrace.com>
2021-03-19 20:01:03 +0100

    Add default values

--- core/src/main/java/org/learn/config/GameConfig.java
+++ core/src/main/java/org/learn/config/GameConfig.java
@@ -3,7 +3,6 @@ package org.learn.config;
 import org.learn.GuessCount;
 import org.learn.MaxNumber;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,10 +11,10 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource(value = "classpath:application.properties")
 public class GameConfig {
 
-    @Value("${game.max-number}")
+    @Value("${game.max-number:100}")
     private int maxNumber;
 
-    @Value("${game.guess-count}")
+    @Value("${game.guess-count:10}")
     private int guessCount;
 
     @Bean
