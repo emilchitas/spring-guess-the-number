372eb55b671b5ecce0934da943e9734e0fda90ec
Emil Chitas <emil.chitas@dynatrace.com>
2021-03-19 19:06:43 +0100

    Use Qualifiers

--- core/src/main/java/org/learn/GameImpl.java
+++ core/src/main/java/org/learn/GameImpl.java
@@ -18,7 +18,10 @@ public class GameImpl implements Game {
     @Autowired
     private NumberGenerator numberGenerator;
 
-    private int guessCount = 10;
+    @Autowired
+    @GuessCount
+    private int guessCount;
+
     private int number;
     private int guess;
     private int smallest;
--- core/src/main/java/org/learn/config/GameConfig.java
+++ core/src/main/java/org/learn/config/GameConfig.java
@@ -1,5 +1,7 @@
 package org.learn.config;
 
+import org.learn.GuessCount;
+import org.learn.MaxNumber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,9 +9,17 @@ import org.springframework.context.annotation.Configuration;
 public class GameConfig {
 
     private int maxNumber = 50;
+    private int guessCount=8;
 
     @Bean
+    @MaxNumber
     public int maxNumber() {
         return maxNumber;
+    }
+
+    @Bean
+    @GuessCount
+    public int guessCount(){
+        return guessCount;
     }
 }
